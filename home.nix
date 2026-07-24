7df663711b114d4897e6e7777ebde85c9cd12bd8
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-07-24 15:35:27 +0200

    Nettoyage

--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@
 
   home.stateVersion = "26.05"; # Please read the comment before changing.
 
-  home.packages = [ ];
+  home.packages = with pkgs; [ kdePackages.kate kdePackages.kdeconnect-kde netflix ytmdesktop klavaro];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -112,5 +112,36 @@
         and echo "🎉 Votre système est parfaitement à jour !"
       '';
     };
+  };
+
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, W, exec, firefox"
+          ", Print, exec, grimblast copy area"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+          builtins.concatLists (builtins.genList (i:
+              let ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
+    };
+  };
+
+  programs.kitty.enable = true;
+
+  services.kdeconnect = {
+    enable = true;
   };
 }
