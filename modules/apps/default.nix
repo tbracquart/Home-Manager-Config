73dce9045a9a1ea20f467f2f8650047482cd4cbd
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-07-25 16:59:10 +0200

    Structure

--- modules/apps/default.nix
+++ modules/apps/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./packages.nix
+    ./git.nix
   ];
 }
--- modules/default.nix
+++ modules/default.nix
@@ -2,10 +2,9 @@
 
 {
   imports = [
+    ./apps/default.nix
     ./desktop/default.nix
     ./files/default.nix
-    ./packages/default.nix
-    ./programs/default.nix
     ./services/default.nix
     ./shell/default.nix
     ./variables/default.nix
