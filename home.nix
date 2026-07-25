8c24b5980ff6ca5a342de2d11aa6808b8091d8ce
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-07-25 16:27:23 +0200

    Structure

--- home.nix
+++ home.nix
@@ -3,20 +3,12 @@
 {
   imports = [
     <plasma-manager/modules>
-    ./modules/desktop/default.nix
-    ./modules/files/default.nix
-    ./modules/packages/default.nix
-    ./modules/programs/default.nix
-    ./modules/services/default.nix
-    ./modules/shell/default.nix
-    ./modules/variables/default.nix
+    ./modules/default.nix
   ];
 
-  # Options globales
   home.username = "thibaut";
   home.homeDirectory = "/home/thibaut";
   home.stateVersion = "26.05";
 
-  # Activation de home-manager lui-même
   programs.home-manager.enable = true;
 }
---
+++ modules/default.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  imports = [
+    ./desktop/default.nix
+    ./files/default.nix
+    ./packages/default.nix
+    ./programs/default.nix
+    ./services/default.nix
+    ./shell/default.nix
+    ./variables/default.nix
+  ];
+}
