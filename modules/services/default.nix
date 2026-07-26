96f638cc1eda6eee2cae76141a2b40c0a02f112a
Thibaut Bracquart <202062783+tbracquart@users.noreply.github.com>
2026-07-26 12:34:50 +0200

    easyeffect + nettoyages services

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,6 +2,6 @@
 
 {
   imports = [
-    ./kdeconnect.nix
+    ./services.nix
   ];
 }
---
+++ modules/services/services.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+
+{
+  services.kdeconnect.enable = true;
+  services.easyeffects.enable = true;
+}
